b744d3b40185230377babd47eed29415313b16ba
akyeshi <akyeshi@outlook.com>
2024-08-05 23:53:20 -0400

    made changes to environment variable

--- app/firebase.js
+++ app/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCUGCsvRKA7G-BkMwfyEnZiegVf5WS4uX8",
-  authDomain: "expense-tracker-3015c.firebaseapp.com",
-  projectId: "expense-tracker-3015c",
-  storageBucket: "expense-tracker-3015c.appspot.com",
-  messagingSenderId: "115036894870",
-  appId: "1:115036894870:web:ec7abd5116586a6bf374b7"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
